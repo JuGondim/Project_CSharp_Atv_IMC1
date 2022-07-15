2c730face36d063862c407eb5b0c9d37c4e345f4
Juliana <juh.ngn@gmail.com>
2022-07-15 17:39:23 -0300

    Inclusão Project Calc_IMC

---
+++ Calculadora_IMC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculadora_IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu nome? ");
+            String nome = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Qual sua idade? ");
+            float idade = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual sua altura? ");
+            float alt = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual seu peso? ");
+            float peso = (float)Convert.ToDouble(Console.ReadLine());
+
+            float imc = peso / (alt * alt);
+
+            Console.WriteLine("Seu IMC é " + Math.Round (imc,1) + ".");
+        }
+    }
+}
